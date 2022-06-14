f7f79c4a077ed8dcb8d0794ee582c56b991df0f1
lanalang <lana.lang.supergirl@gmail.com>
2022-06-15 03:16:49 +0700

    changes message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
+import Header from "./Header/Header.js";
+import Container from "@mui/material/Container";
+import SearchBar from "./SearchBar/SearchBar.js"
 
 function App () {
     return (
       <div className="App">
-        <h1> Hello, Семочка! </h1>
-        <h1> Hello, Семочка! </h1>
-        <h1> Hello, Семочка! </h1>
+        <Header />
+        <Container
+          sx={{
+            height: 200,
+            width: '100%',
+            backgroundColor: 'lightslategrey',
+            display: {md: 'flex' },
+            pt: '88px',
+          }}
+        >
+          <SearchBar />
+        </Container>
       </div>
     );
 }
