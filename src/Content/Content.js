31c4669f449ae3ccda93db13d3ad3b780c0e7bcf
Vladimir Shvets <stormherz@gmail.com>
2022-07-09 00:44:52 +0700

    fixed incorrect brackets, added wrapping fragment with unique key, added empty array

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Container from "@mui/material/Container";
+import Container from '@mui/material/Container';
 import RecipeItem from '../RecipeItem/RecipeItem';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -28,57 +28,57 @@ function getMultipleRandom(arr, num) {
 let randomCategories = getMultipleRandom(categories, 3);
 
 console.log(randomCategories);
-console.log(data.recipes)
+console.log(data.recipes);
 
-    let filtered = {};
+let filtered = {};
 
-    data.recipes.forEach(recipe => {
-        for (let i = 0; i < randomCategories.length; i++) {
-          let cat = randomCategories[i];
-            if (recipe.keywords.indexOf(cat) === -1) {
-              continue;
-            }
-            if (!filtered[cat]) {
-              filtered[cat] = [];
-            }
-            filtered[cat].push(recipe);
-        }
-    });
+data.recipes.forEach((recipe) => {
+  for (let i = 0; i < randomCategories.length; i++) {
+    let cat = randomCategories[i];
+    if (!filtered[cat]) {
+      filtered[cat] = [];
+    }
+
+    if (recipe.keywords.indexOf(cat) === -1) {
+      continue;
+    }
+
+    filtered[cat].push(recipe);
+  }
+});
 console.log(filtered);
 
-
 export default function Content() {
-
-    return (
-      <Container
-        maxWidth="xl"
-        sx={{
-          display: { md: 'flex' },
-          pt: '88px',
-        }}
-      >
-        <Container disableGutters>
-          {randomCategories.map((category) => (
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        display: { md: 'flex' },
+        pt: '88px',
+      }}
+    >
+      <Container disableGutters>
+        {randomCategories.map((category) => (
+          <React.Fragment key={category + 'fragment'}>
             <Typography variant="h4">
               {category}
-              <Box key={category} sx={{ display: 'flex' }}>
-                {filtered[category].map((recipe) => {(
-                      <RecipeItem
-                        key={recipe.id}
-                        title={recipe.title}
-                        src={recipe.images}
-                        subheader={recipe['macros-info']}
-                        alt={recipe.title}
-                        description={recipe.description}
-                        text={recipe.text}
-                      />
-                    )
-                  }
-              )}
-              </Box>
             </Typography>
-          ))}
-        </Container>
+            <Box sx={{ display: 'flex' }}>
+              {filtered[category].map((recipe) => (
+                <RecipeItem
+                  key={category + 'item' + recipe.id}
+                  title={recipe.title}
+                  src={recipe.images}
+                  subheader={recipe['macros-info']}
+                  alt={recipe.title}
+                  description={recipe.description}
+                  text={recipe.text}
+                />
+              ))}
+            </Box>
+          </React.Fragment>
+        ))}
       </Container>
-    );
+    </Container>
+  );
 }
