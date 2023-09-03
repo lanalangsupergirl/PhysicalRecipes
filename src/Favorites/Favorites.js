2ff6ebe89ee7763a35b28885f6a984094c124053
lanalang <lana.lang.supergirl@gmail.com>
2023-09-04 04:11:23 +0700

    favorites page added

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import RecipeItem from '../RecipeItem/RecipeItem';
+import { useSelector, useDispatch } from 'react-redux';
+import { showFullRecipe, currentRecipeId } from '../store/fullRecipeSlice';
+import { Link } from 'react-router-dom';
+import SearchResults from '../SearchResults/SearchResults';
+import { urlImg } from '../utils';
+
+export default function Favorites() {
+
+  let favoritesIds = useSelector((state) => state.favoriteRecipes.favoriteRecipes);
+  let dataRecipes = useSelector((state) => state.dataRecipes.dataRecipes);
+  let allRecipes = dataRecipes;
+
+  let favoritesList = allRecipes.filter((recipe) => {
+    return favoritesIds.includes(recipe.id);
+  });
+
+  const dispatch = useDispatch();
+
+  const flag = useSelector((state) => state.search.flag);
+
+  const handleOnItemClick = React.useCallback(
+    (id) => {
+      dispatch(showFullRecipe(true));
+      dispatch(currentRecipeId(id));
+    },
+    [currentRecipeId, showFullRecipe],
+  );
+
+  return (
+    <>
+      {flag === false ? (
+        <Container
+          disableGutters
+          maxWidth="100%"
+          sx={{
+            display: { md: 'flex' },
+            pt: '55px',
+          }}
+        >
+          <Box>
+            <Typography variant="h4" sx={{ pl: '12px', mb: '15px' }}>
+              ИЗБРАННОЕ
+            </Typography>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+              {favoritesList.map((recipe) => (
+                <Link
+                  to={`/id=${recipe.id}`}
+                  style={{ color: 'inherit', textDecoration: 'inherit' }}
+                  key={'item' + recipe.id}
+                >
+                  <RecipeItem
+                    title={recipe.title}
+                    src={urlImg + recipe.path}
+                    subheader={recipe.macros}
+                    alt={recipe.title}
+                    description={recipe.description}
+                    text={recipe.text}
+                    onItemClick={handleOnItemClick}
+                    id={recipe.id}
+                    categories={recipe.categories}
+                  />
+                </Link>
+              ))}
+            </Box>
+          </Box>
+        </Container>
+      ) : (
+        <SearchResults />
+      )}
+    </>
+  );
+}
